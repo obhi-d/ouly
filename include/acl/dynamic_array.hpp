1ef4419c2d2cb33004280b91f84e87ef97113744
obhi-d <obi.de.online@gmail.com>
2022-12-31 16:18:35 +0100

    Allow fixed size dynamic array

--- include/acl/dynamic_array.hpp
+++ include/acl/dynamic_array.hpp
@@ -9,9 +9,11 @@
 
 namespace acl
 {
+template <typename T, typename Allocator = default_allocator<>, uint32_t N = 0>
+class dynamic_array;
 
-template <typename T, typename Allocator = default_allocator<>>
-class dynamic_array : public Allocator
+template <typename T, typename Allocator>
+class dynamic_array<T, Allocator, 0> : public Allocator
 {
 
 public:
@@ -133,5 +135,123 @@ public:
 private:
   T*       data_  = nullptr;
   uint32_t count_ = 0;
+};
+
+template <typename T, typename Allocator, uint32_t N>
+class dynamic_array : public Allocator
+{
+
+public:
+  static inline constexpr uint32_t count_ = N;
+  static_assert(count_ > 0);
+
+  dynamic_array() = default;
+
+  inline dynamic_array(dynamic_array&& other) noexcept : data_(other.data_)
+  {
+    other.data_ = nullptr;
+  }
+
+  inline dynamic_array& operator=(dynamic_array&& other) noexcept
+  {
+    clear();
+    data_       = other.data_;
+    other.data_ = nullptr;
+    return *this;
+  }
+
+  inline dynamic_array(dynamic_array const& other) noexcept : dynamic_array(other.begin(), other.end()) {}
+
+  inline dynamic_array& operator=(dynamic_array const& other) noexcept
+  {
+    clear();
+    data_ = (T*)Allocator::allocate(sizeof(T) * count_, alignof(T));
+    std::uninitialized_copy_n(other.begin(), count_, data_);
+    return *this;
+  }
+
+  template <typename It>
+  inline dynamic_array(It first, It last) noexcept
+  {
+    auto count = (static_cast<uint32_t>(std::distance(first, last)));
+    data_      = (T*)Allocator::allocate(sizeof(T) * count_, alignof(T));
+    std::uninitialized_copy_n(first, std::min(count, count_), data_);
+    if (count < count_)
+      std::uninitialized_fill_n(data_ + count, count_ - count, T());
+  }
+
+  inline dynamic_array(uint32_t count, T const& fill = T()) noexcept
+  {
+    data_ = (T*)Allocator::allocate(sizeof(T) * count_, alignof(T));
+    std::uninitialized_fill_n(data_, std::min(count, count_), fill);
+    if (count < count_)
+      std::uninitialized_fill_n(data_ + count, count_ - count, T());
+  }
+
+  inline ~dynamic_array() noexcept
+  {
+    clear();
+  }
+
+  void clear() noexcept
+  {
+    if (data_)
+    {
+      if constexpr (!std::is_trivially_destructible_v<T>)
+        std::destroy_n(data_, count_);
+      Allocator::deallocate(data_, sizeof(T) * count_, alignof(T));
+      data_ = nullptr;
+    }
+  }
+
+  T& operator[](size_t i) noexcept
+  {
+    assert(i < count_);
+    return data_[i];
+  }
+
+  T const& operator[](size_t i) const noexcept
+  {
+    assert(i < count_);
+    return data_[i];
+  }
+
+  T* begin() noexcept
+  {
+    return data_;
+  }
+
+  T* end() noexcept
+  {
+    return data_ + count_;
+  }
+
+  T const* begin() const noexcept
+  {
+    return data_;
+  }
+
+  T const* end() const noexcept
+  {
+    return data_ + count_;
+  }
+
+  T* data() noexcept
+  {
+    return data_;
+  }
+
+  T const* data() const noexcept
+  {
+    return data_;
+  }
+
+  uint32_t size() const noexcept
+  {
+    return count_;
+  }
+
+private:
+  T* data_ = nullptr;
 };
 } // namespace acl
--- unit_tests/dynamic_array.cpp
+++ unit_tests/dynamic_array.cpp
@@ -6,18 +6,40 @@
 TEST_CASE("dynamic_array: Validate use of dynamic_array")
 {
   acl::dynamic_array<std::string> table;
-  
+
   table = acl::dynamic_array<std::string>(10, "something");
   for (auto& t : table)
     REQUIRE(t == "something");
-  table    = acl::dynamic_array<std::string>(10, "something_else");
+  table = acl::dynamic_array<std::string>(10, "something_else");
   for (auto& t : table)
     REQUIRE(t == "something_else");
 
   auto other = acl::dynamic_array<std::string>(10, "second");
   for (auto& t : other)
     REQUIRE(t == "second");
-  table    = other;
+  table = other;
+  for (auto& t : other)
+    REQUIRE(t == "second");
+  for (auto& t : table)
+    REQUIRE(t == "second");
+}
+
+TEST_CASE("dynamic_array: Validate use of fixed size  dynamic_array")
+{
+  using darray = acl::dynamic_array<std::string, acl::default_allocator<>, 10>;
+  darray table;
+
+  table = darray(10, "something");
+  for (auto& t : table)
+    REQUIRE(t == "something");
+  table = darray(10, "something_else");
+  for (auto& t : table)
+    REQUIRE(t == "something_else");
+
+  auto other = darray(10, "second");
+  for (auto& t : other)
+    REQUIRE(t == "second");
+  table = other;
   for (auto& t : other)
     REQUIRE(t == "second");
   for (auto& t : table)
