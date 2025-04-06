f142d4d107f1d8a186fb6dcc5808ada1a608e16a
obhi-d <obi.de.online@gmail.com>
2025-04-06 11:55:31 +0200

    Fix leak in sparse vector

--- include/acl/containers/sparse_vector.hpp
+++ include/acl/containers/sparse_vector.hpp
@@ -80,15 +80,9 @@ private:
     return reinterpret_cast<value_type*>(src);
   }
 
-  constexpr static auto cast(storage& src) -> value_type&
-    requires(std::is_same_v<storage, value_type>)
-  {
-    return src;
-  }
+  constexpr static auto cast(storage& src) -> value_type& requires(std::is_same_v<storage, value_type>) { return src; }
 
-  constexpr static auto cast(storage& src) -> value_type&
-    requires(!std::is_same_v<storage, value_type>)
-  {
+  constexpr static auto cast(storage& src) -> value_type& requires(!std::is_same_v<storage, value_type>) {
     // NOLINTNEXTLINE
     return reinterpret_cast<value_type&>(src);
   }
@@ -141,6 +135,7 @@ public:
   ~sparse_vector()
   {
     clear();
+    shrink_to_fit();
   }
 
   struct index_t
@@ -164,56 +159,52 @@ public:
   }
 
   // NOLINTNEXTLINE
-  auto operator=(sparse_vector const& other) noexcept -> sparse_vector&
-    requires(std::is_copy_constructible_v<value_type>)
-  {
-    if (this != &other)
-    {
-      clear();
-      items_.resize(other.items_.size());
-      for (size_type i = 0; i < items_.size(); ++i)
-      {
-        auto const src_storage = other.items_[i];
-        if (src_storage)
-        {
-          items_[i] = acl::allocate<storage>(*this, allocate_bytes, alignarg<Ty>);
+  auto operator=(sparse_vector const& other) noexcept
+   -> sparse_vector& requires(std::is_copy_constructible_v<value_type>) {
+     if (this != &other)
+     {
+       clear();
+       items_.resize(other.items_.size());
+       for (size_type i = 0; i < items_.size(); ++i)
+       {
+         auto const src_storage = other.items_[i];
+         if (src_storage)
+         {
+           items_[i] = acl::allocate<storage>(*this, allocate_bytes, alignarg<Ty>);
 
-          if (src_storage)
-          {
-            if constexpr (std::is_trivially_copyable_v<Ty> || has_pod)
-            {
-              std::memcpy(items_[i], src_storage, allocate_bytes);
-            }
-            else
-            {
-              if constexpr (has_pool_tracking)
-              {
-                pool_occupation(i) = other.pool_occupation(i);
-              }
-              for (size_type e = 0; e < pool_size; ++e)
-              {
-                auto const& src = cast(src_storage[e]);
-                auto&       dst = cast(items_[i][e]);
+           if constexpr (std::is_trivially_copyable_v<Ty> || has_pod)
+           {
+             std::memcpy(items_[i], src_storage, allocate_bytes);
+           }
+           else
+           {
+             if constexpr (has_pool_tracking)
+             {
+               pool_occupation(i) = other.pool_occupation(i);
+             }
+             for (size_type e = 0; e < pool_size; ++e)
+             {
+               auto const& src = cast(src_storage[e]);
+               auto&       dst = cast(items_[i][e]);
 
-                if (!is_null(src))
-                {
-                  std::construct_at(&dst, src);
-                }
-              }
-            }
-          }
-        }
-        else
-        {
-          items_[i] = nullptr;
-        }
-      }
+               if (!is_null(src))
+               {
+                 std::construct_at(&dst, src);
+               }
+             }
+           }
+         }
+         else
+         {
+           items_[i] = nullptr;
+         }
+       }
 
-      static_cast<base_type&>(*this) = static_cast<base_type const&>(other);
-      length_                        = other.length_;
-    }
-    return *this;
-  }
+       static_cast<base_type&>(*this) = static_cast<base_type const&>(other);
+       length_                        = other.length_;
+     }
+     return *this;
+   }
 
   /**
    * @brief Lambda called for each element
@@ -462,6 +453,10 @@ public:
           auto src  = cast(back + length_to_move_in);
           std::move(src, src + length_to_shift, dest);
         }
+      }
+      else
+      {
+        delete_block(back);
       }
     }
     else
@@ -828,9 +823,7 @@ private:
     }
   }
 
-  auto pool_occupation(storage* p) noexcept -> uint32_t&
-    requires(has_pool_tracking)
-  {
+  auto pool_occupation(storage* p) noexcept -> uint32_t& requires(has_pool_tracking) {
     // NOLINTNEXTLINE
     return *reinterpret_cast<uint32_t*>(reinterpret_cast<std::uint8_t*>(p) + pool_bytes);
   }
@@ -842,11 +835,8 @@ private:
     return *reinterpret_cast<uint32_t*>(reinterpret_cast<std::uint8_t const*>(p) + pool_bytes);
   }
 
-  auto pool_occupation(size_type p) noexcept -> uint32_t&
-    requires(has_pool_tracking)
-  {
-    return pool_occupation(items_[p]);
-  }
+  auto pool_occupation(size_type p) noexcept
+   -> uint32_t& requires(has_pool_tracking) { return pool_occupation(items_[p]); }
 
   auto pool_occupation(size_type p) const noexcept -> uint32_t
     requires(has_pool_tracking)
@@ -897,18 +887,24 @@ private:
     }
   }
 
-  void delete_block(size_type block)
+  void delete_block(storage* block)
   {
     if constexpr (!std::is_trivially_destructible_v<value_type> && !has_pod)
     {
       for (size_type i = 0; i < pool_size; ++i)
       {
-        std::destroy_at(std::addressof(cast(items_[block][i])));
+        std::destroy_at(std::addressof(cast(block[i])));
       }
     }
 
-    acl::deallocate(static_cast<allocator_type&>(*this), items_[block], allocate_bytes, alignarg<Ty>);
-    items_[block] = nullptr;
+    acl::deallocate(static_cast<allocator_type&>(*this), block, allocate_bytes, alignarg<Ty>);
+  }
+
+  void delete_block(size_type block)
+  {
+    auto& store = items_[block];
+    delete_block(store);
+    store = nullptr;
   }
 
   /**
