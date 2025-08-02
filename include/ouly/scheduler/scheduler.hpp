08c4c46c8dd81a3c05f237afed7e408c8d546dcb
obhi-d <obi.de.online@gmail.com>
2025-08-02 17:55:10 +0200

    Remove redundant async APIs  .

--- include/ouly/scheduler/scheduler.hpp
+++ include/ouly/scheduler/scheduler.hpp
@@ -27,24 +27,6 @@ void async(WC const& current, Args&&... args)
 }
 
 /**
- * @brief Submits a task asynchronously to the scheduler
- *
- * @tparam M Work item type to be submitted
- * @tparam Args Variable template parameter pack for work item arguments
- *
- * @param current Current worker context that will submit the work
- * @param args Arguments to be forwarded to the work item constructor
- *
- * @note This is a helper function that forwards the submission request to the scheduler
- *       associated with the current worker context
- */
-template <auto M, TaskContext WC, typename... Args>
-void async(WC const& current, Args&&... args)
-{
-  current.get_scheduler().template submit<M>(current, std::forward<Args>(args)...);
-}
-
-/**
  * @brief Asynchronously submits a task to the scheduler
  *
  * This function forwards the given arguments to the scheduler's submit function,
@@ -61,25 +43,6 @@ template <TaskContext WC, typename... Args>
 void async(WC const& current, workgroup_id submit_group, Args&&... args)
 {
   current.get_scheduler().submit(current, submit_group, std::forward<Args>(args)...);
-}
-
-/**
- * @brief Submits a task asynchronously to the scheduler
- *
- * @tparam M Work item type to be submitted
- * @tparam Args Variable template parameter pack for work item arguments
- *
- * @param current Current worker context that will submit the work
- * @param submit_group Target workgroup ID where the work will be submitted
- * @param args Arguments to be forwarded to the work item constructor
- *
- * @note This is a helper function that forwards the submission request to the scheduler
- *       associated with the current worker context
- */
-template <auto M, TaskContext WC, typename... Args>
-void async(WC const& current, workgroup_id submit_group, Args&&... args)
-{
-  current.get_scheduler().template submit<M>(current, submit_group, std::forward<Args>(args)...);
 }
 
 } // namespace ouly
